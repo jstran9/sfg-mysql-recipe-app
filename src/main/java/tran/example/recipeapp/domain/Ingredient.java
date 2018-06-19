237afd659d0ad8f4421f91b221b19350a9c53bff
todd <toddtran9@gmail.com>
2018-06-18 18:11:31 -0700

    Creating many-to-one relationship.

---
+++ src/main/java/tran/example/recipeapp/domain/Ingredient.java
@@ -0,0 +1,53 @@
+package tran.example.recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    // private UnitOfMeasure uom;
+
+    // no cascade b/c we don't want to cascade upward to delete the recipe.
+    // we want to freely delete ingredients assigned to the recipe.
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
