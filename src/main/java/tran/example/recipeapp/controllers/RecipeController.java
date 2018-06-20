35729a5048f2f98571ecc156a9818ebb855eb3bd
todd <toddtran9@gmail.com>
2018-06-20 13:54:13 -0700

    Creating a mapping to show the recipe creation form and for save and update operations.

--- src/main/java/tran/example/recipeapp/controllers/RecipeController.java
+++ src/main/java/tran/example/recipeapp/controllers/RecipeController.java
@@ -3,8 +3,11 @@ package tran.example.recipeapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tran.example.recipeapp.commands.RecipeCommand;
 import tran.example.recipeapp.services.RecipeService;
 
 @Controller
@@ -24,5 +27,17 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findRecipeById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/new")
+    public String createRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:" + RECIPE_BASE_URL + "/show/" + savedRecipeCommand.getId();
     }
 }
