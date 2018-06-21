a63839a0aa053a3223da1b7149ff40aa6567c7be
todd <toddtran9@gmail.com>
2018-06-20 17:24:36 -0700

    Adding in a request mapper to handle showing an ingredient of a given recipe.

--- src/main/java/tran/example/recipeapp/controllers/IngredientController.java
+++ src/main/java/tran/example/recipeapp/controllers/IngredientController.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tran.example.recipeapp.services.IngredientService;
 import tran.example.recipeapp.services.RecipeService;
 
 @Slf4j
@@ -13,16 +15,27 @@ import tran.example.recipeapp.services.RecipeService;
 public class IngredientController {
 
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
     @Autowired
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
+    @GetMapping
     @RequestMapping(RecipeController.RECIPE_BASE_URL + "/{recipeId}" + "/ingredients")
     public String getIngredients(@PathVariable String recipeId, Model model) {
         log.debug("getting ingredients for recipe with id: " + recipeId);
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping
+    @RequestMapping(RecipeController.RECIPE_BASE_URL + "/{recipeId}" + "/ingredient" + "/{ingredientId}" + "/show")
+    public String showIngredient(@PathVariable String recipeId, @PathVariable String ingredientId, Model model) {
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId),
+                Long.valueOf(ingredientId)));
+        return "recipe/ingredient/show";
     }
 }
