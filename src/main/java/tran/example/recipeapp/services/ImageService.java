13a6ebce22f676e69bff56d8bf475a27818a67f7
todd <toddtran9@gmail.com>
2018-06-21 01:06:04 -0700

    Creating the stub to save the image.

---
+++ src/main/java/tran/example/recipeapp/services/ImageService.java
@@ -0,0 +1,7 @@
+package tran.example.recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
