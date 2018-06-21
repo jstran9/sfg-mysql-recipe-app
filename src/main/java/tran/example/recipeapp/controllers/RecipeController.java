74b5ed2ec86f882a83dc6b02ecdaf2f1545433bb
todd <toddtran9@gmail.com>
2018-06-21 14:18:59 -0700

    Using a Spring MVC Exception Handler annotation which takes precedence.

--- src/main/java/tran/example/recipeapp/controllers/RecipeController.java
+++ src/main/java/tran/example/recipeapp/controllers/RecipeController.java
@@ -2,10 +2,13 @@ package tran.example.recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import tran.example.recipeapp.commands.RecipeCommand;
+import tran.example.recipeapp.exceptions.NotFoundException;
 import tran.example.recipeapp.services.RecipeService;
 
 @Slf4j
@@ -51,5 +54,14 @@ public class RecipeController {
         log.debug("Deleting id: " + id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 }
