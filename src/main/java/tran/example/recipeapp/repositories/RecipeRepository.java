35d81b18706c9565a78499e5e21164e018ac35ca
todd <toddtran9@gmail.com>
2018-06-22 18:58:20 -0700

    Adding in a method declaration to help the new MySQL bootstrap file find recipe by direction and name (directions).

--- src/main/java/tran/example/recipeapp/repositories/RecipeRepository.java
+++ src/main/java/tran/example/recipeapp/repositories/RecipeRepository.java
@@ -3,5 +3,8 @@ package tran.example.recipeapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import tran.example.recipeapp.domain.Recipe;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+    Optional<Recipe> findByDescriptionAndDirections(String description, String directions);
 }
